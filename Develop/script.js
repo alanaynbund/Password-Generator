dc234a891714c47c37036525a3c59148a8d35208
Stanley <aynbunda@gmail.com>
2021-05-04 13:28:10 -0400

    started declarations and generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,44 @@
 // Assignment code here
-
+var abcLow = "abcdefghijklmnopqrstuvwxyz";
+var abcUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "1234567890";
+var symbols = "!@#$%^&*()-_=+{[}]|<>?";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var passLength = parseInt(
+    prompt("Choose a password length between 8 and 128 characters")
+  );
+  
+  if (isNaN(passLength) === true) {
+    alert("Password length must be a number");
+    return;
+  }
+
+  if (passLength < 8) {
+    alert ("Password length must be at least 8 characters!");
+    return;
+  }
+
+  var pass = ""
+
+  var passUp = confirm("Would you like uppercase letters");
+    if (passUp) {
+      pass= pass + abcUp;
+      console.log(pass);
+    }
+}
+
+
+
 // Write password to the #password input
-function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
